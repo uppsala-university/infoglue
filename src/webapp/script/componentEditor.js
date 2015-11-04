55d5cb34ddc95b1dd5c6f45526a71e675c2ad207
rasmusUppsala <rasmus.ljungstrom@uadm.uu.se>
2015-11-04 15:53:10 +0100

    debug

--- src/webapp/script/componentEditor.js
+++ src/webapp/script/componentEditor.js
@@ -2554,7 +2554,7 @@ function viewSource()
 	
 	function changeLanguage(siteNodeId, languageId, contentId)
 	{
-		alert("languageID " + languageId);
+		alert("languageID " + languageId.value);
 		window.location.href = "ViewPage!renderDecoratedPage.action?siteNodeId=" + siteNodeId + "&languageId=" + languageId.value + "&contentId=" + contentId + "";
 	}
 	
