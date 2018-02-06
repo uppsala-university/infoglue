54710cc6374bb0a68f9d2dfb282360353b2f4236
ioVulture <rasmus.ljungstrom@uadm.uu.se>
2018-02-06 09:45:52 +0100

    Made the code a bit simpler

--- src/java/org/infoglue/cms/applications/common/actions/WebworkAbstractAction.java
+++ src/java/org/infoglue/cms/applications/common/actions/WebworkAbstractAction.java
@@ -707,24 +707,23 @@ public abstract class WebworkAbstractAction implements Action, ServletRequestAwa
 				final String tid = getGeneralSetting(GA_CMS_ID, null);
 				final String gaUrl = getGeneralSetting(GA_CMS_URL, null);
 				final String actionFinal = action;
-	    		boolean isExcludedAction = false;
-	    		
-    			String excludedActions = getGeneralSetting(GA_EXLUDED_ACTIONS, null);
-	    		if (excludedActions != null) {	
-	    			isExcludedAction = excludedActions.matches(".*(^|,)" + action + "(,|$).*");
-	    		}
-	    		System.out.println("1:" + action + "," + tid + "," + isExcludedAction);
+				boolean isExcludedAction = false;
+			
+				String excludedActions = getGeneralSetting(GA_EXLUDED_ACTIONS, null);
+				if (excludedActions != null) {	
+					isExcludedAction = excludedActions.matches(".*(^|,)" + action + "(,|$).*");
+				}
+			
 				if (!isExcludedAction && action != null && tid != null && !tid.equalsIgnoreCase("") && gaUrl != null && !gaUrl.equalsIgnoreCase("")) {
-					System.out.println("2:");
+				
 					Thread thread = new Thread(new Runnable() {
 						public void run() {
 							sendToGA(actionFinal, userName, tid, gaUrl);
 						}
 					});
 					thread.start(); 
-					
 				}
-	    		
+			
 				logger.debug("action: " + action + ", method: " + method + ", context: " + context + ", userName: " + userName + ", parameters: " + parameters);
 
 				// Some actions are called too often, exclude them.
@@ -772,10 +771,10 @@ public abstract class WebworkAbstractAction implements Action, ServletRequestAwa
 		// Send analytics data with post to google analytics measurement protocol
 		String urlParameters  = "";
 		urlParameters = "v=1&tid=" + tid + "&cid=" + session.getAttribute("GASession") + "&t=event&ec=" + principalRole + "&ea=" + action;
-		System.out.println("3:" + urlParameters);
+		
 		byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
 		String request        = gaUrl;
-		System.out.println("4:" + gaUrl);
+		
 		URL url;
 		try {
 			url = new URL( request );
@@ -792,7 +791,7 @@ public abstract class WebworkAbstractAction implements Action, ServletRequestAwa
 			System.out.println("5:" + conn.getResponseCode());
 			} catch (IOException e) {
 		
-			logger.warn("Could send analytics data for action:" + action + " and data:" + postData);
+			logger.warn("Could not send analytics data for action:" + action + " and data:" + postData);
 		}
 		
 	}
