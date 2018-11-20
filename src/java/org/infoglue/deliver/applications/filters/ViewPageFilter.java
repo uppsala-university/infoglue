fc97744c02a7463627fdf2120a0d14eb21b28a3b
ioVulture <rasmus.ljungstrom@uadm.uu.se>
2018-11-20 09:08:27 +0100

    trying to only go with one path step

--- src/java/org/infoglue/deliver/applications/filters/ViewPageFilter.java
+++ src/java/org/infoglue/deliver/applications/filters/ViewPageFilter.java
@@ -515,8 +515,8 @@ public class ViewPageFilter implements Filter
         if(firstPath.startsWith("/"))
         	firstPath = firstPath.substring(1);
         String[] splitPath = firstPath.split("/");
-        if(splitPath.length > 1)
-        	firstPath = splitPath[0] + "/" + splitPath[1];
+        
+        firstPath = splitPath[0];
 
         if (firstPath.endsWith("/")) {
         	firstPath = firstPath.substring(0, firstPath.lastIndexOf("/"));
